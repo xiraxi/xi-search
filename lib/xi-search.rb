acfca3886d6ffa73f9a376cdaf5e04e1809d1924
Ayose <ayose@foton.es>
2010-09-17 02:22:26 +0100

    Model helpers to search
    Every model has a has_text_search macro to define which fields should be used
    to filter by text, and a with_words scope.

--- lib/xi-search.rb
+++ lib/xi-search.rb
@@ -1,3 +1,9 @@
 
 class XiSearch < Rails::Engine
 end
+
+ActiveSupport.on_load(:active_record) do
+  require 'xi-search/models'
+  extend XiSearch::ModelHelpers
+end
+
---
+++ lib/xi-search/models.rb
@@ -0,0 +1,23 @@
+module XiSearch::ModelHelpers
+
+  # TODO Use MakeTextSearch when finishied
+  def has_text_search(*fields)
+    return @has_text_search if fields.empty?
+    @has_text_search ||= []
+    @has_text_search.concat fields.map {|field_name| field_name.to_s }
+
+    @has_text_search
+  end
+
+  def has_text_search?
+    !!@has_text_search
+  end
+
+  def with_words(words)
+    fields = @has_text_search || columns.map {|column| column.text? ? column.name : nil }.compact
+    query_by_word = fields.map {|field| "#{connection.quote_column_name field} ILIKE :word" }.join(" OR ")
+
+    words.to_s.split.inject(self) {|query, word| query.where(query_by_word, :word => "%#{word}%") }
+  end
+
+end
